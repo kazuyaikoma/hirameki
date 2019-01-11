fff93d72626478caec3f8ea729a4aded1d84fc92
KAZUYA IKOMA <audu817@gmail.com>
2019-01-11 22:15:50 +0900

    update FinalVC

--- BrainBot/Home/FinalViewController.swift
+++ BrainBot/Home/FinalViewController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 import Foundation
+import UICircularProgressRing
 
 class FinalViewController: UIViewController {
+    @IBOutlet weak var progressCircle: UICircularProgressRing!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var themeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.updateView()
+        self.progressCircle.gradientColors = [UIColor.red, UIColor.blue]
+        self.themeLabel.numberOfLines = 0
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // TODO: 完成度を修正
+        self.progressCircle.startProgress(to: 100, duration: 2.0)
     }
     
 }
