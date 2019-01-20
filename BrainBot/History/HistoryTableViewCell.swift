e55486fc07384015f7cca3733dd172b7f1fbd4f1
KAZUYA IKOMA <audu817@gmail.com>
2019-01-20 18:35:13 +0900

    add bg image, HistoryTableViewCell

---
+++ BrainBot/History/HistoryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryTableViewCell.swift
+//  BrainBot
+//
+//  Created by KazuyaIkoma on 2019/01/20.
+//  Copyright © 2019 kaz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
