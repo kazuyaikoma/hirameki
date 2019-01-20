094990073c4a0a2fe9b254f7f333fec18aedb14c
KAZUYA IKOMA <audu817@gmail.com>
2019-01-20 19:59:34 +0900

    made HistoryTableViewCell updatable

--- BrainBot/History/HistoryTableViewCell.swift
+++ BrainBot/History/HistoryTableViewCell.swift
@@ -10,8 +10,25 @@ import Foundation
 import UIKit
 
 class HistoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var year: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var theme: UILabel!
+    @IBOutlet weak var idea: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+    }
+    
+    func update(date: Date, theme: String, idea: String) {
+        let yearFormatter = DateFormatter()
+        let dateFormatter = DateFormatter()
+        yearFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "Yyyy", options: 0, locale: Locale(identifier: "en_US"))
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "Md", options: 0, locale: Locale(identifier: "en_US"))
+        
+        self.year.text = yearFormatter.string(from: date)
+        self.date.text = dateFormatter.string(from: date)
+        
+        self.theme.text = theme
+        self.idea.text = idea
     }
 }
